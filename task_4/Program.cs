9cf7a0ef18b5fd49033aa1c676f0b3819f648fc1
Boris <uzumak1.naruto228@mail.ru>
2023-09-22 19:11:05 +0300

    Четвертое задание готово

---
+++ task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter_plus = 2;
+int counter_minus = -2;
+// для положительных чисел
+if (num >= 2) 
+{
+    do {
+    Console.Write(counter_plus + " ");
+    counter_plus = counter_plus + 2;
+} while (counter_plus <= num);
+}
+//для отрицательных чисел
+else if (num <= -2)
+{
+    do{
+        Console.Write(counter_minus + " ");
+        counter_minus = counter_minus - 2;
+    } while (counter_minus >= num);
+}
+//для неправильных чисел
+else Console.Write("В указаном диапазоне нет четных чисел(");
