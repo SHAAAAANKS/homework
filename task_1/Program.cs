82d769db45afe5cd2a5988c6c55722de0fbd4835
Boris <uzumak1.naruto228@mail.ru>
2023-09-22 17:35:50 +0300

    первое задание выполнено

---
+++ task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введи второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+if (num1 > num2) Console.WriteLine("число " + num1 + " больше чем число " + num2 );
+else if (num1 < num2) Console.WriteLine("число " + num2 + " больше чем число " + num1 );
+else Console.WriteLine("число " + num1 + " равно числу " + num2 );
