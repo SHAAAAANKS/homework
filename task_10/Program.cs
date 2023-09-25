132d15c86fa4bb8fb17c03049a8944355dedfe68
Boris <uzumak1.naruto228@mail.ru>
2023-09-25 17:20:11 +0300

    первое задание выполнено

---
+++ task_10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введи трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи еще трехзначное число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи еще трехзначное число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+//Хочу создать метод, чтоб попрактиковаться
+int FindSecond(int arg)
+{
+    int result = (arg / 10) % 10;
+    return result;
+}
+
+Console.WriteLine($"вторая цифра числа {num} -> {FindSecond(num)}");
+Console.WriteLine($"вторая цифра числа {num2} -> {FindSecond(num2)}");
+Console.WriteLine($"вторая цифра числа {num3} -> {FindSecond(num3)}");
